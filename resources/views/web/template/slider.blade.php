34477141b4ea5dbd4bb00890b05666727e4b2894
zenetare45@gmail.com <zenetare45@gmail.com>
2020-08-03 22:41:56 +0100

    laravel blog

--- resources/views/web/template/slider.blade.php
+++ resources/views/web/template/slider.blade.php
@@ -1,5 +1,13 @@
 <div class="slider-bar width-pc margin-rp">
-    <div class="post-slider"></div>
-    <div class="post-slider"></div>
-    <div class="post-slider"></div>
+    <div class="ca-right post-slider-right">
+        <div class="blog-slider-right width-all height-all">
+            <img class="width-all height-all" src="https://1.bp.blogspot.com/-IMm8aIR8c9U/XygZi5wfFhI/AAAAAAAAWcw/sflBoofJNecKXzkcwbDFX9nw34fsH5v1QCLcBGAsYHQ/w340/image3_-_2020-08-03T150018.767_600x400.jpg">
+        </div>
+    </div>
+    <div class="ca-left post-slider-left">
+        <div class="blog-slider-left"></div>
+        <div class="blog-slider-left"></div>
+        <div class="blog-slider-left"></div>
+        <div class="blog-slider-left"></div>
+    </div>
 </div>
