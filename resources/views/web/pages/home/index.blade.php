0397ec004cf499d405073b56201d98c4ce329cf9
zenetare45@gmail.com <zenetare45@gmail.com>
2020-08-05 16:53:26 +0100

    laravel blog

--- resources/views/web/pages/home/index.blade.php
+++ resources/views/web/pages/home/index.blade.php
@@ -4,8 +4,27 @@
 @section('content')
 
     @include('web.template.slider')
-    <div class="content-body">
-        
+    <div class="content-home width-all">
+        <div class="width-pc margin-rp side-post">
+            <div class="head-content width-all">
+                <h2>أخر الأخبار</h2>
+            </div>
+            <div class="width-all pa-20 body-content">
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="post-blog"></div>
+                <div class="clr"></div>
+            </div>
+        </div>
     </div>
 
 @endsection
